1483f95f8cf6788e2a67b46140d8a8b73e71233e
Erik Ostermueller <eostermueller@gmail.com>
2016-06-12 08:55:13 -0500

    fixes for issues #1 and #2

--- db/loadDb.sh
+++ db/loadDb.sh
@@ -5,5 +5,5 @@ export JMETER_PLAN_01=$dir/loadDb-01.jmx
 export JMETER_PLAN_02=$dir/loadDb-02.jmx
 
 
-$JMETER_HOME/bin/jmeter.sh -n -t $JMETER_PLAN_01
-$JMETER_HOME/bin/jmeter.sh -n -t $JMETER_PLAN_02
+$JMETER_HOME/bin/jmeter.sh -Juser.classpath=$dir/h2/bin -n -t $JMETER_PLAN_01
+$JMETER_HOME/bin/jmeter.sh -Juser.classpath=$dir/h2/bin -n -t $JMETER_PLAN_02
--- db/startDb.sh
+++ db/startDb.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 dir=$(dirname "$0")
 
+## The loadDb.sh script will create 200mb of data in the db/data folder.  
+## To change the location, change BASE_DIR environment variable (below)
+## to an existing folder where you want the data to be created.
+
 export BASE_DIR=$dir/data
-#export BASE_DIR=$dir/data2
 #export BASE_DIR=/Volumes/eto-external-drive/h2/data
 
 
-#$JAVA_HOME/bin/java -cp $dir/h2/bin/h2*.jar org.h2.tools.Server -ifExists -baseDir $BASE_DIR -tcpAllowOthers
 $JAVA_HOME/bin/java -cp $dir/h2/bin/h2*.jar org.h2.tools.Server -baseDir $BASE_DIR
 
 
