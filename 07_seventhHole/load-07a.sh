31c9f2752c8d77f748bf83e613dedfa2d524bdc0
Erik Ostermueller <eostermueller@gmail.com>
2016-06-15 23:52:44 -0500

    partially complete with 07

--- 07_seventhHole/load-07a.sh
+++ 07_seventhHole/load-07a.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 dir=$(dirname "$0")
 
-export JMETER_PLAN=$dir/x07a.jmx
+export JMETER_PLAN=$dir/x07d.jmx
 
 $JMETER_HOME/bin/jmeter.sh -n -t $JMETER_PLAN
--- 07_seventhHole/load-07d.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-dir=$(dirname "$0")
-
-export JMETER_PLAN=$dir/x07d.jmx
-
-$JMETER_HOME/bin/jmeter.sh -n -t $JMETER_PLAN
