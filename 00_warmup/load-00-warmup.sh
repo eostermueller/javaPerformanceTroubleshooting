ca8217fa62ebaf0694ff4bbf988c92a4d639023d
Erik Ostermueller <eostermueller@gmail.com>
2016-06-27 08:12:11 -0500

    parameterizing SCENARIO_NUM

--- 00_warmup/load-00-warmup.sh
+++ 00_warmup/load-00-warmup.sh
@@ -3,4 +3,4 @@ dir=$(dirname "$0")
 . $dir/../bin/setenv.sh
 export JMETER_PLAN=$dir/x00_warmup.jmx
 
-$JMETER_HOME/bin/jmeter.sh -n -t $JMETER_PLAN
+$JMETER_HOME/bin/jmeter.sh -JSCENARIO_NUM=2 -n -t $JMETER_PLAN
