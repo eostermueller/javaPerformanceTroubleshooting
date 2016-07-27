c5d523b59fcdc9d08f6b39e7462dd81dca804ba5
Erik Ostermueller <eostermueller@gmail.com>
2016-07-27 05:37:07 -0500

    now using maven to download h2

--- bin/setenv.cmd
+++ bin/setenv.cmd
@@ -1,4 +1,8 @@
-set H2_JAR=h2-1.4.192.jar
+
+SET bin_dir=%~dp0
+SET bin_dir=%bin_dir:~0,-1%
+
+set H2_JAR=%bin_dir%\..\warProject\target\performanceGolf\WEB-INF\lib\h2-1.4.191.jar
 set JMETER_HOME=C:\Users\winSquee\Desktop\jmeter\apache-jmeter-3.0
 
 set JAVA_HOME=C:\Progra~1\Java\jdk1.8.0_91
--- db/startDb.sh
+++ db/startDb.sh
@@ -8,8 +8,10 @@ dir=$(dirname "$0")
 #export BASE_DIR=$dir/data
 export BASE_DIR=/Volumes/eto-external-drive/h2/wideData
 
+export H2=$dir/../warProject/target/performanceGolf/WEB-INF/lib/h2-1.4.191.jar
 
-$JAVA_HOME/bin/java -cp $dir/h2/bin/h2*.jar org.h2.tools.Server -baseDir $BASE_DIR
+#$JAVA_HOME/bin/java -cp $dir/h2/bin/h2*.jar org.h2.tools.Server -baseDir $BASE_DIR
+$JAVA_HOME/bin/java -cp $H2 org.h2.tools.Server -baseDir $BASE_DIR
 
 
 #[-baseDir <dir>]        The base directory for H2 databases (all servers)
