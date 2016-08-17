896079fbd0181ccdd26be24f7074b50f355ac05a
Erik Ostermueller <eostermueller@gmail.com>
2016-08-17 07:49:06 -0500

    fixing path to h2

--- db/loadDb.cmd
+++ db/loadDb.cmd
@@ -7,5 +7,5 @@ SET JMETER_PLAN_01=%dir%\loadDb-01.jmx
 SET JMETER_PLAN_02=%dir%\loadDb-02.jmx
 
 
-CALL %JMETER_HOME%\bin\jmeter.bat -Juser.classpath=%dir%\h2\bin -n -t %JMETER_PLAN_01%
-CALL %JMETER_HOME%\bin\jmeter.bat -Juser.classpath=%dir%\h2\bin -n -t %JMETER_PLAN_02%
+CALL %JMETER_HOME%\bin\jmeter.bat -Juser.classpath=%dir%\..\warProject\target\performanceGolf\WEB-INF\lib\ -n -t %JMETER_PLAN_01%
+CALL %JMETER_HOME%\bin\jmeter.bat -Juser.classpath=%dir%\..\warProject\target\performanceGolf\WEB-INF\lib\ -n -t %JMETER_PLAN_02%
--- db/loadDb.sh
+++ db/loadDb.sh
@@ -5,5 +5,5 @@ export JMETER_PLAN_01=$dir/loadDb-01.jmx
 export JMETER_PLAN_02=$dir/loadDb-02.jmx
 
 
-$JMETER_HOME/bin/jmeter.sh -Juser.classpath=$dir/h2/bin -n -t $JMETER_PLAN_01
-$JMETER_HOME/bin/jmeter.sh -Juser.classpath=$dir/h2/bin -n -t $JMETER_PLAN_02
+$JMETER_HOME/bin/jmeter.sh -Juser.classpath=$dir/../warProject/target/performanceGolf/WEB-INF/lib/ -n -t $JMETER_PLAN_01
+$JMETER_HOME/bin/jmeter.sh -Juser.classpath=$dir/../warProject/target/performanceGolf/WEB-INF/lib/ -n -t $JMETER_PLAN_02
