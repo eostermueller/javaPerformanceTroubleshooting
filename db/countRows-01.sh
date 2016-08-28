1a7cc0cd465f7fe2cc41b968704ef0b6d8fda946
Erik Ostermueller <eostermueller@gmail.com>
2016-08-28 14:35:40 -0500

    avoiding manual h2 download

--- db/countRows-01.sh
+++ db/countRows-01.sh
@@ -3,4 +3,4 @@ dir=$(dirname "$0")
 
 export JMETER_PLAN=$dir/countRows-01.jmx
 
-$JMETER_HOME/bin/jmeter.sh -Juser.classpath=$dir/h2/bin -n -t $JMETER_PLAN
+$JMETER_HOME/bin/jmeter.sh -Juser.classpath=$dir/../warProject/target/performanceGolf/WEB-INF/lib/ -n -t $JMETER_PLAN
--- db/helpDb.sh
+++ db/helpDb.sh
@@ -5,6 +5,8 @@ dir=$(dirname "$0")
 export BASE_DIR=$dir/data2
 #export BASE_DIR=/Volumes/eto-external-drive/h2
 
-java -cp $dir/h2/bin/h2*.jar org.h2.tools.Server -help
+export H2=$dir/../warProject/target/performanceGolf/WEB-INF/lib/h2-1.4.191.jar
+
+java -cp $H2 org.h2.tools.Server -help
 
 
