6979e440ded299df39dfe486de91435020faa942
Erik Ostermueller <eostermueller@gmail.com>
2016-08-29 22:59:28 -0500

    switch from perfSandbox.war to performanceGolf.war

--- 03_thirdHole/startPsWar-03.sh
+++ 03_thirdHole/startPsWar-03.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 dir=$(dirname "$0")
 
-export PS_WAR=$dir/../warProject/target/perfSandbox.war
+export PG_WAR=$dir/../warProject/target/performanceGolf.war
 . $dir/../bin/setenv.sh
 
 #export MY_AGENT=-javaagent:/Users/erikostermueller/Documents/src/jdist/jdbc-performance-logger/JDBC-Performance-Logger-0.8.0/jdbc-perf-logger-agent/target/jdbc-perf-logger-agent-0.8.0.jar
 
-$JAVA_HOME/bin/java $MY_AGENT -jar $PS_WAR
+$JAVA_HOME/bin/java $MY_AGENT -jar $PG_WAR
