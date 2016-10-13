68aadc110a6542fc0515a0ec8174cbd96a8a330c
Erik Ostermueller <eostermueller@gmail.com>
2016-10-12 23:40:13 -0500

    including -D parm for SAX optimization

--- 10_tenthHole/startPgWar-10.cmd
+++ 10_tenthHole/startPgWar-10.cmd
@@ -5,4 +5,5 @@ call %dir%\..\bin\setenv.cmd
 
 SET PG_WAR=%dir%\..\warProject\target\performanceGolf.war
 
-%JAVA_HOME%\bin\java -jar %PG_WAR%
+REM %JAVA_HOME%\bin\java -jar %PG_WAR% -Djavax.xml.parsers.SAXParserFactory=com.sun.org.apache.xerces.internal.jaxp.SAXParserFactoryImpl
+%JAVA_HOME%\bin\java -jar %PG_WAR% 
--- 10_tenthHole/startPgWar-10.sh
+++ 10_tenthHole/startPgWar-10.sh
@@ -5,4 +5,5 @@ export PG_WAR=$dir/../warProject/target/performanceGolf.war
 . $dir/../bin/setenv.sh
 
 
-$JAVA_HOME/bin/java -jar $PG_WAR
+#$JAVA_HOME/bin/java -jar $PG_WAR -Djavax.xml.parsers.SAXParserFactory=com.sun.org.apache.xerces.internal.jaxp.SAXParserFactoryImpl
+$JAVA_HOME/bin/java -jar $PG_WAR 
