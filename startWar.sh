cb3181063ec2d72977c74ca61122cd1c2e2d1498
Erik Ostermueller <eostermueller@gmail.com>
2017-05-13 08:06:20 -0500

    removed mvn debugw

--- startWar.sh
+++ startWar.sh
@@ -1,1 +1,3 @@
-mvn -f pom-startWar.xml -P$1 exec:exec
+#This scripts takes a single parameter, the 3-character id of the test.
+#Each id is a profile in pom-startWar.xml
+mvn -f pom-startWar.xml -P$1 clean deploy
