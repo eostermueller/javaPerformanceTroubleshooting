0218cf7cdc3bb89ba04f7f00d18c6cf57f2a064c
Erik Ostermueller <eostermueller@gmail.com>
2017-06-04 09:13:31 -0500

    fix for issue #3, and better logging

--- init.sh
+++ init.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 dir=$(dirname "$0")
 
+export LOG=$dir/init.log
+
+echo "### init.sh start" >> $LOG
+date >> $LOG
 echo "@@"
 echo "@@"
 echo "@@ About to initialize the performanceGolf war & db."
@@ -14,7 +18,16 @@ echo "@@ 3) Make sure Maven 3 or greater is installed and MAVEN_HOME/bin is in t
 echo "@@"
 read -p "@@ Press any key to continue, or Ctrl+C to exit."
 
-export LOG=$dir/db/h2.log
+echo .
+echo .
+echo .
+echo "Please wait."
+echo "To follow progress of this install:"
+echo "1) open a separate terminal window to this folder: $PWD"
+echo "2) run this command:  tail -f init.log"
+echo .
+echo .
+echo .
 
 chmod +x load.sh
 chmod +x startWar.sh
@@ -24,12 +37,14 @@ chmod +x db/countRows.sh
 chmod +x db/helpDb.sh
 
 #Build the WAR file used for all performance testing
-mvn -f warProject/pom.xml clean package
+echo "### WAR file build" >> $LOG
+date >> $LOG
+mvn -f warProject/pom.xml clean package 1>> $LOG 2>&1
 
 ###
 ###    Delete any existing database
 ###
-$dir/db/stopDb.sh 1> /dev/null 2>&1
+$dir/db/stopDb.sh 1>> $LOG 2>&1
 rm -rf $dir/db/data
 mkdir $dir/db/data
 
@@ -37,14 +52,24 @@ mkdir $dir/db/data
 ###    Create blank DB and generate data into it.
 ###    This is the part that takes 5-10 minutes.
 ###
-$dir/db/startDb.sh 1> $LOG 2>&1 &
-$dir/load.sh loadDb-01
-$dir/load.sh loadDb-02
+echo "### Starting DB" >> $LOG
+date >> $LOG
+$dir/db/startDb.sh 1>> $LOG 2>&1 &
+sleep 5
+echo "### After DB start" >> $LOG
+date >> $LOG
+
+echo "### DB Load 01" >> $LOG
+date >> $LOG
+$dir/load.sh loadDb-01 1>> $LOG 2>&1
+echo "### DB Load 02" >> $LOG
+date >> $LOG
+$dir/load.sh loadDb-02 1>> $LOG 2>&1
 echo "@@"
 echo "@@ Here are row counts of tables:"
 echo "@@"
-$dir/load.sh countRows-01.jmx | grep S0
-$dir/load.sh countRows-02.jmx | grep S0
+$dir/load.sh countRows-01 | grep S0
+$dir/load.sh countRows-02 | grep S0
 $dir/db/stopDb.sh
 
 echo "@@"
