3323381b440ae21991df9ff0e4ba96e5ede7cfaf
Adam Clemons <adam@adamclmns.com>
2017-06-10 11:48:14 -0500

    Removing JAVA_HOME reference from Start and Stop db scripts for windows.

--- db/stopDb.cmd
+++ db/stopDb.cmd
@@ -2,4 +2,4 @@ SET dir_stop=%~dp0
 SET dir_stop=%dir_stop:~0,-1%
 
 
-%JAVA_HOME%\bin\java -cp %dir_stop%\..\warProject\target\performanceGolf\WEB-INF\lib\h2-1.4.191.jar org.h2.tools.Server -tcpShutdown tcp://localhost:9092
+java -cp %dir_stop%\..\warProject\target\performanceGolf\WEB-INF\lib\h2-1.4.191.jar org.h2.tools.Server -tcpShutdown tcp://localhost:9092
