19891f71111a30418a4db39bb821c7a23fe33ba0
Erik Ostermueller <eostermueller@gmail.com>
2017-08-14 07:55:16 -0500

    browser no longer launching to admin screen

--- db/startDb.cmd
+++ db/startDb.cmd
@@ -4,4 +4,5 @@ SET dir_db=%dir_db:~0,-1%
 
 SET BASE_DIR=%dir_db%\data
 
-java -cp %dir_db%\..\warProject\target\performanceGolf\WEB-INF\lib\h2-1.4.191.jar org.h2.tools.Server -baseDir %BASE_DIR%
+REM %JAVA_HoME%\bin\java -cp %dir_db%\..\warProject\target\performanceGolf\WEB-INF\lib\h2-1.4.191.jar org.h2.tools.Server -baseDir %BASE_DIR%
+java -cp %dir_db%\..\warProject\target\performanceGolf\WEB-INF\lib\h2-1.4.191.jar org.h2.tools.Server -tcp -web -baseDir %BASE_DIR%
