380ef295c968537cce5c1a570854312c057d8257
Erik Ostermueller <eostermueller@gmail.com>
2017-09-30 01:05:23 -0500

    using script dir to find pom.xml file

--- startWar.sh
+++ startWar.sh
@@ -1,3 +1,5 @@
 #This scripts takes a single parameter, the 3-character id of the test.
 #Each id is a profile in pom-startWar.xml
-mvn -f pom-startWar.xml -P$1 clean deploy
+
+BASEDIR=$(dirname $0)
+mvn -f $BASEDIR/pom-startWar.xml -P$1 clean deploy
